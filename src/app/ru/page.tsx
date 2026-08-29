1240af54a9aa612b50bbb4d50a202e67c9eee0b7
ordoyanedvard-art <ordoyanedvard@gmail.com>
2026-08-29 11:52:35 +0300

    Update page.tsx

--- src/app/ru/page.tsx
+++ src/app/ru/page.tsx
@@ -1,5 +1,5 @@
-import HomePage from "../page";
+import PageShell from "@/components/PageShell";
 
 export default function RussianPage() {
-  return <HomePage />;
+  return <PageShell locale="ru" />;
 }
