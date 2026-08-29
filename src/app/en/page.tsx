766657cad53564297b731793f8839590258293b7
ordoyanedvard-art <ordoyanedvard@gmail.com>
2026-08-29 11:53:04 +0300

    Update page.tsx

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -1,5 +1,5 @@
-import HomePage from "../page";
+import PageShell from "@/components/PageShell";
 
 export default function EnglishPage() {
-  return <HomePage />;
+  return <PageShell locale="en" />;
 }
