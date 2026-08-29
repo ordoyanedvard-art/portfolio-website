1879aea63cca50fdfebe883bf54827a55888c6af
ordoyanedvard-art <ordoyanedvard@gmail.com>
2026-08-29 21:07:16 +0300

    Update works.ts

--- src/data/works.ts
+++ src/data/works.ts
@@ -2,6 +2,8 @@ import type { Work } from "@/lib/types";
 
 export const works: Work[] = [];
 
+export const workSlugs = works.map((work) => work.slug);
+
 export function getWorkBySlug(slug: string) {
   return works.find((work) => work.slug === slug);
 }
